7ed975df0b1fa146c5ca0fb9220016b158a15b54
FloatingComet62 <shubh2007rai@gmail.com>
2023-03-18 17:13:09 +0530

    s-kit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<main>
+  <header>Aargh Rai</header>
+  Aargh Rai is a programmer from India who has been been in the field since 2017
+</main>
+<section>
+  <header>Languages I have experienced</header>
+  <div>Typescript</div>
+  <div>Rust</div>
+  <div>C</div>
+  <div>Python</div>
+  <div>C#</div>
+  <div>Javascript</div>
+  <div>C++</div>
+</section>
+<section>
+  <header>My Projects</header>
+  <div>Comrade</div>
+  <div>CometOS</div>
+  <div>FluidDB</div>
+  <div>ChessUI</div>
+  <div>PainScript</div>
+  <div>todo</div>
+</section>
