89ebe2036bc35e4a0baadb8310e6da76a6b93a2d
FloatingComet62 <shubh2007rai@gmail.com>
2024-10-27 18:57:19 +0000

    rewrite in plain html

--- components/Bold.tsx
+++
@@ -1,7 +0,0 @@
-import { FunctionComponent, ReactNode } from 'react';
-
-const Bold: FunctionComponent<{ children: ReactNode }> = ({ children }) => {
-    return <b className='text-[#f5fc2f]'>{children}</b>
-}
-
-export default Bold;
